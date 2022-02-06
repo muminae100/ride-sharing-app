8df0eda32f5c270b75ee4f372e511b1442eec18f
muminae100 <smuminaetx100@gmail.com>
2022-02-06 19:07:06 +0300

    Function to get current user location

--- app.js
+++ app.js
@@ -18,6 +18,45 @@ function initMap() {
     title: "Hello World!",
   });
 
+  
+var infoWindow = new google.maps.InfoWindow();
+const locationButton = document.createElement("button");
+
+locationButton.textContent = "Find me";
+
+locationButton.classList.add("custom-map-control-button");
+map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
+locationButton.addEventListener('click', function() {
+// set default map location where the use is located
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        const marker = new google.maps.Marker({
+          position: pos,
+          map: map,
+        });  
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Me');
+        infoWindow.open(map, marker);
+        map.setCenter(pos);
+      },
+      () => {
+        handleLocationError(true, infoWindow, map.getCenter());
+      }
+    );
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+}
+
+});
 
 }
 
